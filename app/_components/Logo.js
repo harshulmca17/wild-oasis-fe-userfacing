4788bc26f2977314a9671f6cae0dde60628f48b2
Harshul <harshulk47@gmail.com>
2024-06-01 19:56:27 +0530

    Update

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import logo from "@/public/logo.png";
 import Image from "next/image";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image height="60" width="60" src="/logo.png" alt="The Wild Oasis logo" />
+      <Image height="60" width="60" src={logo} alt="The Wild Oasis logo" />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>

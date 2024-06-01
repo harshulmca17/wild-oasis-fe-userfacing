ffcbbcaa088e9e0ee92835e61d85ce9fdb93f480
Harshul <harshulk47@gmail.com>
2024-06-01 19:05:22 +0530

    Update

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,21 +1,12 @@
 import Counter from "../_components/Counter";
+export const metadata = {
+  title: "Cabins",
+};
 
 export default async function Page() {
-  const res = await fetch("http://hk-alb2-47b2d838154cd58f.elb.ap-south-1.amazonaws.com/guests");
-  const data = await res.json();
-
-
   return (
     <div>
       <h1>Cabins page</h1>
-
-      <ul>
-        {data.result.map((user) => (
-          <li key={user.id}>{user.fullName}</li>
-        ))}
-      </ul>
-
-      <Counter users={data?.result ?? []} />
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,39 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
-import "@/app/_styles/globals.css"
+
+import { Josefin_Sans } from "next/font/google";
+
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
+const josefin_Sans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-josefin-sans",
+  display: "swap",
+});
+
+
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcom | The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin_Sans.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
